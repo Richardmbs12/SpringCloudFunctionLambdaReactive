7e13529674ddf9c687f482e3fb7f3bdca39ed1d0
Richard Hudson <richard.hudson@standardbank.co.za>
2021-03-04 18:05:22 +0200

    Issue 2 - java.lang.ClassCastException: class reactor.core.publisher.FluxPeekFuseable cannot be cast to class org.springframework.messaging.Message

--- src/main/java/test/TestApplication.java
+++ src/main/java/test/TestApplication.java
@@ -1,22 +1,22 @@
-//package test;
-//
-//import org.springframework.boot.SpringApplication;
-//import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.context.annotation.Bean;
-//
-//import java.util.function.Function;
-//
-//import reactor.core.publisher.Flux;
-//
-//@SpringBootApplication
-//public class TestApplication {
-//
-//    public static void main(final String[] args) {
-//        SpringApplication.run(TestApplication.class);
-//    }
-//
-//    @Bean
-//    public Function<Flux<String>, Flux<String>> uppercase() {
-//        return stringFlux -> stringFlux.map(String::toUpperCase);
-//    }
-//}
+package test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.function.Function;
+
+import reactor.core.publisher.Flux;
+
+@SpringBootApplication
+public class TestApplication {
+
+    public static void main(final String[] args) {
+        SpringApplication.run(TestApplication.class);
+    }
+
+    @Bean
+    public Function<Flux<String>, Flux<String>> uppercase() {
+        return stringFlux -> stringFlux.map(String::toUpperCase);
+    }
+}
